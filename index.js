aeef23880d29d3764557b144d3a53b9f9b66611a
michaelrhodes <spam@michaelrhod.es>
2015-01-16 13:42:53 +1000

    Polyfill Test.prototype._end for tape@1

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var tape = require('tape')
 
 exports = module.exports = tape
+
+// Maintain tape@1 compatibility
+exports.Test.prototype._end = (
+  exports.Test.prototype._end ||
+  exports.Test.prototype.end
+)
+
 exports.Test.prototype.run = function () {
   if (!this._cb || this._skip) {
     return this._end()
   }
-
   this.emit('prerun')
-
   try {
     this._cb(this)
   }
@@ -16,6 +21,5 @@ exports.Test.prototype.run = function () {
     this._end()
     return
   }
-
   this.emit('run') 
 }
