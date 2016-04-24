1019c0cd3016caf2d5fd5b1a68311a1116a4a0f9
michaelrhodes <spam@michaelrhod.es>
2016-04-23 23:46:53 -0700

    Fix catch-exceptions test

--- test/catch-exceptions.js
+++ test/catch-exceptions.js
@@ -22,9 +22,9 @@ test('catch exceptions', function (t) {
               var tests = (/# tests\s+([0-9]+)/.exec(results) || [])[1];
               var pass = (/# pass\s+([0-9]+)/.exec(results) || [])[1];
               var fail = (/# fail\s+([0-9]+)/.exec(results) || [])[1];
-              t.equal(tests, '2');
+              t.equal(tests, '4');
               t.equal(pass, '1'); 
-              t.equal(fail, '1'); 
+              t.equal(fail, '3'); 
             }));
         });
     });
